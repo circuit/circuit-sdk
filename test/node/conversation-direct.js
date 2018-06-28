92aad1562df6391b33d8fedfd8f1f164220a4bd6
Richard <richard.fazzio@unify.com>
2018-06-28 09:06:09 -0400

    Add conversation-direct.js and conversation-group.js

---
+++ test/node/conversation-direct.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const assert = require('assert');
+const Circuit = require('../../circuit-node');
+const config = require('./config.json');
+const helper = require('./helper');
+Circuit.logger.setLevel(Circuit.Enums.LogLevel.Error);
+
+let client;
+let user;
+let client2;
+let user2;
+let conversation;
+describe('Direct Conversation', () => {
+
+    before(async () => {
+        client = new Circuit.Client(config.bot1);
+        user = await client.logon();
+        client2 = new Circuit.Client(config.bot2);
+        user2 = await client2.logon();
+    });
+
+    after(async () => {
+        await client.logout();
+        await client2.logout();
+    });
+
+    it('should create a direct conversation', async () => {
+        const res = await client.createDirectConversation(user2.userId);
+        conversation = res.conversation;
+        conversation.participants.forEach(userId => {
+            if (!(userId === user.userId || userId === user2.userId)) {
+                assert(false);
+            }
+        });
+    });
+
+    it('should get the direct conversation by its Id', async () => {
+        const res = await client.getConversationById(conversation.convId);
+        assert(res && res.convId === conversation.convId && res.participants.includes(user.userId) && res.participants.includes(user2.userId));
+    });
+
+    it('should get the direct conversation by its email', async () => {
+        const res = await client.getDirectConversationWithUser(user2.emailAddress);
+        if (!res || res.type !== Circuit.Enums.ConversationType.DIRECT) {
+            assert(false);
+        }
+        res.participants.forEach(userId => {
+            if (!(userId === user.userId || userId === user2.userId)) {
+                assert(false);
+            }
+        });
+    });
+
+    it('should get the details of the conversation', async () => {
+        const res = await client.getConversationDetails(conversation.convId);
+        assert(res.conversationCreatorId === user.userId || res.conversationCreatorId === user2.userId);
+    });
+});
