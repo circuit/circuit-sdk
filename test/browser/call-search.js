203d95901f8cf2393b40b3af26a8217b5e4645d6
Fazzio <rfazzio@fau.edu>
2018-09-18 17:17:20 -0400

    Add call searching tests

---
+++ test/browser/call-search.js
@@ -0,0 +1,69 @@
+'use strict';
+
+import { PeerUser } from '../peer-user.js';
+import { expectEvents, updateRemoteVideos, sleep, logEvents } from '../helper.js';
+import config from './config.js'
+
+const assert = chai.assert;
+let client;
+let peerUser;
+let call;
+describe('Call Search', async function() {
+    this.timeout(300000);
+
+    before(async function() {
+        Circuit.logger.setLevel(Circuit.Enums.LogLevel.Error);
+        client = new Circuit.Client(config.config);
+        const res = await Promise.all([PeerUser.create(), PeerUser.create(), client.logon(config.credentials)]);
+        peerUser = res[0];
+        const conversation = await client.createGroupConversation([peerUser.userId], 'SDK Test: Conference Call');
+        call = await client.startConference(conversation.convId, {audio: true, video: false});
+        await expectEvents(client, [{
+            type: 'callStatus',
+            predicate: evt => evt.call.state === Circuit.Enums.CallStateName.Initiated
+        }, {
+            type: 'callStatus',
+            predicate: evt => evt.call.state === Circuit.Enums.CallStateName.Waiting
+        }]);
+        await sleep(5000); // wait to make sure the call is ready to be joined
+        await Promise.all([
+            peerUser.exec('joinConference', call.callId, {audio: true, video: false}),
+            expectEvents(client, [{
+                type: 'callStatus',
+                predicate: evt => evt.reason === 'callStateChanged' && evt.call.state === Circuit.Enums.CallStateName.Active
+            }, {
+                type: 'callStatus',
+                predicate: evt => evt.reason === 'participantJoined'
+            }])
+        ]);
+        call = await client.findCall(call.callId);
+    });
+
+    after(async function() {
+        await Promise.all([peerUser.destroy(), client.logout()]);
+    });
+
+    afterEach(async function() {
+        client.removeAllListeners();
+    });
+
+    it('should get the active call', async () => {
+        const res = await client.getActiveCall();
+        assert(res.callId === call.callId);
+    });
+
+    it('should get all calls and verify it contains the active call', async () => {
+        const res = await client.getCalls();
+        assert(res && res.some(c => c.callId === call.callId));
+    });
+
+    it('should get the call containing the user', async () => {
+        const res = await client.findCall(call.callId);
+        assert(res.participants.some(user => user.userId === peerUser.userId));
+    });
+
+    it('should get last rtp stats', async () => {
+        const res = await client.getLastRtpStats(call.callId);
+        assert(res.some(stat => stat.pcType === 'AUDIO/VIDEO'));
+    });
+});
