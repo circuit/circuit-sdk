eb101478a5bce000dd89a519c7cf86ff86c6a367
Fazzio <rfazzio@fau.edu>
2018-09-21 11:42:46 -0400

    Add tests for call devices

--- test/browser/call-device.js
+++ test/browser/call-device.js
@@ -6,9 +6,11 @@ import config from './config.js'
 
 const assert = chai.assert;
 let client;
-let peerUser, peerUser2;
+let peerUser;
 let call;
-let device;
+let audioInputDevice;
+let audioOutputDevice;
+let videoInputDevice;
 let user;
 describe('Video Sharing', async function() {
     this.timeout(300000);
@@ -16,11 +18,10 @@ describe('Video Sharing', async function() {
     before(async function() {
         Circuit.logger.setLevel(Circuit.Enums.LogLevel.Error);
         client = new Circuit.Client(config.config);
-        const res = await Promise.all([PeerUser.create(), PeerUser.create(), client.logon(config.credentials)]);
+        const res = await Promise.all([PeerUser.create(), client.logon(config.credentials)]);
         peerUser = res[0];
-        peerUser2 = res[1];
-        user = res[2];
-        const conversation = await client.createGroupConversation([peerUser.userId, peerUser2.userId], 'SDK Test: Conference Call');
+        user = res[1];
+        const conversation = await client.createGroupConversation([peerUser.userId], 'SDK Test: Conference Call');
         call = await client.startConference(conversation.convId, {audio: true, video: false});
         await expectEvents(client, [{
             type: 'callStatus',
@@ -41,34 +42,61 @@ describe('Video Sharing', async function() {
     });
 
     after(async function() {
-        await peerUser.exec('endCall', call.callId);
-        await client.endCall(call.callId);
-        await Promise.all([peerUser.destroy(), peerUser2.destroy(), client.logout()]);
+        await Promise.all([peerUser.destroy(), client.logout()]);
     });
 
     afterEach(async function() {
         client.removeAllListeners();
     });
 
-    it('should get started calls', async () => {
-        const res = await peerUser2.exec('getStartedCalls');
-        assert(res.some(c => c.callId === call.callId));
-    })
+    it('should get media devices', async () => {
+        let mediaDevices = await navigator.mediaDevices.enumerateDevices();
+        videoInputDevice = mediaDevices.find(d => d.kind === 'videoinput');
+        audioInputDevice = mediaDevices.find(d => d.kind === 'audioinput');
+        audioOutputDevice = mediaDevices.find(d => d.kind === 'audiooutput');
+        assert(videoInputDevice && audioInputDevice && audioOutputDevice);
+    });
+
+    it('should mute mic', async () => {
+        if (!audioInputDevice) {
+            console.log('No audio input device found.');
+            assert(true);
+            return;
+        } 
+    });
+
+    it('should mute speaker', async () => {
+        if (!audioOutputDevice) {
+            console.log('No audio output device found.');
+            assert(true);
+            return;
+        } 
+    });
+
+    it('should get max video resolution', async () => {
+        if (!videoInputDevice) {
+            console.log('No video input device found.');
+            assert(true);
+            return;
+        }
+        const res = await client.getMaxVideoResolution(videoInputDevice.deviceId);
+        assert(Object.keys(Circuit.Enums.VideoResolution).some(resolution => resolution === res));
+    });
 
     it('should get media devices and confirm the correct client', async () => {
         const res = await client.getDevices();
         assert(res.some(dev => dev.clientId === user.clientId));
-    })
+    });
 
     it('should get audio and video stats', async () => {
         const res = await client.getAudioVideoStats();
-        console.log('audio/vid', res.type);
+        console.log('audio/vid', res.kind);
         assert(res);
     });    
     
     it('should get local audio and video stream', async () => {
         const res = await client.getLocalAudioVideoStream();
-        console.log('stats/vid', res.type);
+        console.log('stream', res.kind);
         assert(res);
     });
 });
