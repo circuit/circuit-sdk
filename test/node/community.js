a33d56f092565407f570a2e8637c05868e5fd496
Fazzio <rfazzio@fau.edu>
2018-10-09 18:52:33 -0400

    Add unit test for new joinCommunity Api.

--- test/node/community.js
+++ test/node/community.js
@@ -6,17 +6,30 @@ const prep = require('../preparation');
 Circuit.logger.setLevel(Circuit.Enums.LogLevel.Error);
 
 let client;
-let user
+let client2;
+let user2;
+let community;
 describe('Community', () => {
     before(async () => {
         client = prep.client;
-        user = client.loggedOnUser;
+        client2 = prep.client2;
+        user2 = client2.loggedOnUser;
     });
 
     it('function: createCommunity', async () => {
         const topic = `${Date.now()}a`;
         const description = `${Date.now()}b`;
-        const res = await client.createCommunity(null, topic, description);
-        assert(res && res.type === Circuit.Enums.ConversationType.COMMUNITY && res.topic === topic && res.description === description);
+        community = await client.createCommunity(null, topic, description);
+        assert(community && community.type === Circuit.Enums.ConversationType.COMMUNITY && community.topic === topic && community.description === description);
+    });
+
+    it('function: joinCommunity', async () => {
+        if (!client2.joinCommunity) {
+            console.log('API not yet supported');
+            assert(true);
+            return;
+        }
+        const res = await client2.joinCommunity(community.convId);
+        assert(res && res.participants.some(participant => participant.userId === user2.userId));
     });
 });
