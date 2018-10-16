8d7915bab8505ef8b72503e59f954b360d369618
Fazzio <rfazzio@fau.edu>
2018-10-16 15:00:43 -0400

    Add test for user stream objects.

--- test/browser/video-sharing.js
+++ test/browser/video-sharing.js
@@ -6,7 +6,8 @@ import config from './config.js'
 
 const assert = chai.assert;
 let client;
-let peerUser;
+let peerUser1;
+let peerUser2;
 let call;
 describe('Video Sharing', async function() {
     this.timeout(300000);
@@ -14,10 +15,12 @@ describe('Video Sharing', async function() {
     before(async function() {
         Circuit.logger.setLevel(Circuit.Enums.LogLevel.Error);
         client = new Circuit.Client(config.config);
-        const res = await Promise.all([PeerUser.create(), client.logon(config.credentials)]);
-        peerUser = res[0];
-        const conversation = await client.createGroupConversation([peerUser.userId], 'SDK Test: Video Sharing');
+        const res = await Promise.all([PeerUser.create(), PeerUser.create(), client.logon(config.credentials)]);
+        peerUser1 = res[0];
+        peerUser2 = res[1];
+        const conversation = await client.createGroupConversation([peerUser1.userId, peerUser2.userId], 'SDK Test: Video Sharing');
         call = await client.startConference(conversation.convId, {audio: true, video: false});
+        console.log(call);
         await expectEvents(client, [{
             type: 'callStatus',
             predicate: evt => evt.call.state === Circuit.Enums.CallStateName.Initiated
@@ -27,7 +30,8 @@ describe('Video Sharing', async function() {
         }]);
         await sleep(5000); // wait to make sure the call is ready to be joined
         await Promise.all([
-            peerUser.exec('joinConference', call.callId, {audio: true, video: false}),
+            peerUser1.exec('joinConference', call.callId, {audio: true, video: true}),
+            peerUser2.exec('joinConference', call.callId, {audio: true, video: true}),
             expectEvents(client, [{
                 type: 'callStatus',
                 predicate: evt => evt.reason === 'participantJoined'
@@ -37,7 +41,7 @@ describe('Video Sharing', async function() {
     });
 
     after(async function() {
-        await Promise.all([peerUser.destroy(), client.logout()]);
+        await Promise.all([peerUser1.destroy(), peerUser2.destroy(), client.logout()]);
     });
 
     afterEach(async function() {
@@ -53,6 +57,16 @@ describe('Video Sharing', async function() {
                 predicate: evt => evt.reason === 'callStateChanged' && evt.call.localVideoUrl.length
             }])
         ]);
+    });
+
+    it('function: findCall, verifies stream objects of users', async () => {
+        await sleep(3000);
+        const res = await Promise.all([
+            client.findCall(call.callId),
+            peerUser1.exec('findCall', call.callId),
+            peerUser2.exec('findCall', call.callId)
+        ]);
+        assert(res.every(userCall => userCall.localStreams.video && userCall.participants.every(participant => participant.streams.video)));
     });
 
     it('function: changeHDVideo [ON], raises event: callStatus with reason: sdpConnected', async () => {
