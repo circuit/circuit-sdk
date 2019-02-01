9b563f4c6303dde1ac08bb5b130d4f51752cc01f
Fazzio <rfazzio@fau.edu>
2019-02-01 16:02:23 -0500

    Fix search tests

--- test/node/search.js
+++ test/node/search.js
@@ -12,14 +12,6 @@ let client2;
 let user2;
 let conversation;
 let searchResults = {} // hashtable of search results from basicSearchResults
-let searchId; // results returned from searchStatus event for startBasicSearch
-let searchId2; // results returned from searchStatus event for startUserSearch by name
-let searchId3; // results returned from searchStatus event for startUserSearch by phone number
-let searchId4; // results returned from searchStatus event for startAdvancedUserSearch by name (searchExactAssignedPhoneNumber = undefined)
-let searchId5; // results returned from searchStatus event for startAdvancedUserSearch by phone number (searchExactAssignedPhoneNumber = false)
-let searchId6; // results returned from searchStatus event for startAdvancedUserSearch by correct phone number (searchExactAssignedPhoneNumber = true)
-let searchId7; // results returned from searchStatus event for startAdvancedUserSearch by incorrect phone number (searchExactAssignedPhoneNumber = true)
-let searchId8; // results returned from searchStatus event for startAdvancedUserSearch by MEETING POINT
 let phoneNumber;
 const MEETING_POINT = {
     query: 'unit-test-dont',
@@ -39,37 +31,42 @@ describe('Search Tests', () => {
         await client2.logout();
     });
 
+    afterEach(async () => await helper.sleep(500));// wait to allow time for searches to return
+
     it('function: startBasicSearch [conversation], with event: searchStatus', async () => {
         const res = await Promise.all([
             client.startBasicSearch(conversation.topic),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'FINISHED'
             }])
         ]);
-        searchId = res[1].data && res[1].data.searchId;
-        assert(searchId);
+        const searchId = res[1].data && res[1].data.searchId;
+        assert(searchResults[searchId] && searchResults[searchId].type === 'basicSearchResults' && searchResults[searchId].data.searchResults.some(conv => conv.convId === conversation.convId));
     });
 
     it('function: startUserSearch [firstName], with event: searchStatus', async () => {
         const res = await Promise.all([
             client.startUserSearch(user2.firstName),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'FINISHED'
             }])
         ]);
-        searchId2 = res[1].data && res[1].data.searchId;
-        assert(searchId2);
+        const searchId = res[1].data && res[1].data.searchId;
+        assert(searchResults[searchId] && searchResults[searchId].type === 'basicSearchResults' && searchResults[searchId].data.users.includes(user2.userId));
     });
 
     it('function: startUserSearch [phoneNumber], with event: searchStatus', async () => {
         const res = await Promise.all([
             client.startUserSearch(phoneNumber.phoneNumber),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'FINISHED'
             }])
         ]);
-        searchId3 = res[1].data && res[1].data.searchId;
-        assert(searchId3);
+        const searchId = res[1].data && res[1].data.searchId;
+        assert(searchResults[searchId] && searchResults[searchId].type === 'basicSearchResults' && searchResults[searchId].data.users.includes(user2.userId));
     });
 
     it('function: startAdvancedUserSearch [name], with event: searchStatus and searchExactAssignedPhoneNumber = undefined', async () => {
@@ -86,11 +83,12 @@ describe('Search Tests', () => {
         const res = await Promise.all([
             client.startAdvancedUserSearch(searchObj),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'FINISHED'
             }])
         ]);
-        searchId4 = res[1].data && res[1].data.searchId;
-        assert(searchId4);
+        const searchId = res[1].data && res[1].data.searchId;
+        assert(searchResults[searchId] && searchResults[searchId].type === 'basicSearchResults' && searchResults[searchId].data.users.includes(user2.userId));
     });
 
     it('function: startAdvancedUserSearch [phoneNumber], with event: searchStatus and searchExactAssignedPhoneNumber = false', async () => {
@@ -106,11 +104,12 @@ describe('Search Tests', () => {
         const res = await Promise.all([
             client.startAdvancedUserSearch(searchObj),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'FINISHED'
             }])
         ]);
-        searchId5 = res[1].data && res[1].data.searchId;
-        assert(searchId5);
+        const searchId = res[1].data && res[1].data.searchId;
+        assert(searchResults[searchId] && searchResults[searchId].type === 'basicSearchResults' && searchResults[searchId].data.users.includes(user2.userId));
     });
 
     it('function: startAdvancedUserSearch [phoneNumber], with event: searchStatus and searchExactAssignedPhoneNumber = true and correct number', async () => {
@@ -127,11 +126,12 @@ describe('Search Tests', () => {
         const res = await Promise.all([
             client.startAdvancedUserSearch(searchObj),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'FINISHED'
             }])
         ]);
-        searchId6 = res[1].data && res[1].data.searchId;
-        assert(searchId6);
+        const searchId = res[1].data && res[1].data.searchId;
+        assert(searchResults[searchId] && searchResults[searchId].type === 'basicSearchResults' && searchResults[searchId].data.users.includes(user2.userId));
     });
 
     it('function: startAdvancedUserSearch [phoneNumber], with event: searchStatus and searchExactAssignedPhoneNumber = true and incorrect number', async () => {
@@ -148,11 +148,10 @@ describe('Search Tests', () => {
         const res = await Promise.all([
             client.startAdvancedUserSearch(searchObj),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'NO_RESULT'
             }])
         ]);
-        searchId7 = res[1].data && res[1].data.searchId;
-        assert(searchId7);
     });
 
     it('function: startAdvancedUserSearch [MEETING_POINT], with event: searchStatus', async () => {
@@ -168,73 +167,15 @@ describe('Search Tests', () => {
         const res = await Promise.all([
             client.startAdvancedUserSearch(searchObj),
             helper.expectEvents(client, [{
-                type: 'searchStatus'
+                type: 'searchStatus',
+                predicate: evt => evt.data.status === 'FINISHED'
             }])
         ]);
-        searchId8 = res[1].data && res[1].data.searchId;
-        assert(searchId8);
-    });
-
-    it('event: basicSearchResults [firstName]', async () => {
-        // wait to allow time for searches to return
-        await helper.sleep(3000);
-        assert(searchResults[searchId2] && searchResults[searchId2].type === 'basicSearchResults' && searchResults[searchId2].data.users.includes(user2.userId));
-    });
-
-    it('event: basicSearchResults [phoneNumber]', async () => {
-        assert(searchResults[searchId3] && searchResults[searchId3].type === 'basicSearchResults' && searchResults[searchId3].data.users.includes(user2.userId));
-    });
-
-    it('event: basicSearchResults [conversation]', async () => {
-        assert(searchResults[searchId] && searchResults[searchId].type === 'basicSearchResults' && searchResults[searchId].data.searchResults.some(conv => conv.convId === conversation.convId));
-    });
-
-    it('event: basicSearchResults [startAdvancedUserSearch, firstName], with searchExactAssignedPhoneNumber = false', async () => {
-        if (!client.startAdvancedUserSearch) {
-            console.log('API not yet supported');
-            assert(true);
-            return;
-        }
-        assert(searchResults[searchId4] && searchResults[searchId4].type === 'basicSearchResults' && searchResults[searchId4].data.users.includes(user2.userId));
-    });
-
-    it('event: basicSearchResults [startAdvancedUserSearch, phoneNumber], with searchExactAssignedPhoneNumber = false', async () => {
-        if (!client.startAdvancedUserSearch) {
-            console.log('API not yet supported');
-            assert(true);
-            return;
-        }
-        assert(searchResults[searchId5] && searchResults[searchId5].type === 'basicSearchResults' && searchResults[searchId5].data.users.includes(user2.userId));
-    });
-
-    it('event: basicSearchResults [startAdvancedUserSearch, phoneNumber], with searchExactAssignedPhoneNumber = true, and correct phone number', async () => {
-        if (!client.startAdvancedUserSearch) {
-            console.log('API not yet supported');
-            assert(true);
-            return;
-        }
-        assert(searchResults[searchId6] && searchResults[searchId6].type === 'basicSearchResults' && searchResults[searchId6].data.users.includes(user2.userId));
-    });
-
-    it('event: basicSearchResults [startAdvancedUserSearch, phoneNumber], with searchExactAssignedPhoneNumber = true, and incorrect phone number', async () => {
-        if (!client.startAdvancedUserSearch) {
-            console.log('API not yet supported');
-            assert(true);
-            return;
-        }
-        assert(!searchResults[searchId7]);
-    });
-
-    it('event: basicSearchResults [startAdvancedUserSearch, MEETING_POINT]', async () => {
-        if (!client.startAdvancedUserSearch) {
-            console.log('API not yet supported');
-            assert(true);
-            return;
-        }
+        const searchId = res[1].data && res[1].data.searchId;
         let users = [];
-        if (searchResults[searchId8]) {
-            users = await client.getUsersById(searchResults[searchId8].data.users);
+        if (searchResults[searchId]) {
+            users = await client.getUsersById(searchResults[searchId].data.users);
         }
-        assert(searchResults[searchId8] && users.some(user => user.userType === Circuit.Enums.SearchContext.MEETING_POINT && user.location === MEETING_POINT.location));
+        assert(searchResults[searchId] && users.some(user => user.userType === Circuit.Enums.SearchContext.MEETING_POINT && user.location === MEETING_POINT.location));
     });
 });
