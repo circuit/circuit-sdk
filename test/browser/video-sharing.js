4d0fa8a5c528a0da52f91befe6aebfa685ecbd75
richardfazzio <richardfazzio@fau.edu>
2019-12-15 18:21:38 -0500

    Remove left in console logs in comments

--- test/browser/video-sharing.js
+++ test/browser/video-sharing.js
@@ -12,29 +12,29 @@ describe('Video Sharing', async function() {
     this.timeout(300000);
 
     // before(async function() {
-        // Circuit.logger.setLevel(Circuit.Enums.LogLevel.Error);
-        // client = new Circuit.Client(config.config);
-        // const res = await Promise.all([PeerUser.create(), client.logon(config.credentials)]);
-        // peerUser1 = res[0];
-        // const conversation = await client.createGroupConversation([peerUser1.userId], 'SDK Test: Video Sharing');
-        // call = await client.startConference(conversation.convId, {audio: true, video: false});
-        // await expectEvents(client, [{
-        //     type: 'callStatus',
-        //     predicate: evt => evt.call.state === Circuit.Enums.CallStateName.Initiated
-        // }, {
-        //     type: 'callStatus',
-        //     predicate: evt => evt.call.state === Circuit.Enums.CallStateName.Waiting
-        // }]);
-        // await sleep(5000); // wait to make sure the call is ready to be joined
-        // await Promise.all([
-        //     peerUser1.exec('joinConference', call.callId, {audio: true, video: false}),
-        //     expectEvents(client, [{
-        //         type: 'callStatus',
-        //         predicate: evt => evt.reason === 'participantJoined'
-        //     }])
-        // ]);
-        // call = await client.findCall(call.callId);
-        // document.querySelector('#localVideo').srcObject = call.localVideoStream;
+    //     Circuit.logger.setLevel(Circuit.Enums.LogLevel.Error);
+    //     client = new Circuit.Client(config.config);
+    //     const res = await Promise.all([PeerUser.create(), client.logon(config.credentials)]);
+    //     peerUser1 = res[0];
+    //     const conversation = await client.createGroupConversation([peerUser1.userId], 'SDK Test: Video Sharing');
+    //     call = await client.startConference(conversation.convId, {audio: true, video: false});
+    //     await expectEvents(client, [{
+    //         type: 'callStatus',
+    //         predicate: evt => evt.call.state === Circuit.Enums.CallStateName.Initiated
+    //     }, {
+    //         type: 'callStatus',
+    //         predicate: evt => evt.call.state === Circuit.Enums.CallStateName.Waiting
+    //     }]);
+    //     await sleep(5000); // wait to make sure the call is ready to be joined
+    //     await Promise.all([
+    //         peerUser1.exec('joinConference', call.callId, {audio: true, video: false}),
+    //         expectEvents(client, [{
+    //             type: 'callStatus',
+    //             predicate: evt => evt.reason === 'participantJoined'
+    //         }])
+    //     ]);
+    //     call = await client.findCall(call.callId);
+    //     document.querySelector('#localVideo').srcObject = call.localVideoStream;
     // });
 
     // after(async function() {
@@ -51,33 +51,13 @@ describe('Video Sharing', async function() {
 
 
     // it('function: toggleVideo [ON], raises event: callStatus with reason: callStateChanged', async () => {
-    //     const res = await Promise.all([
+    //     await Promise.all([
     //         client.toggleVideo(call.callId),
     //         expectEvents(client, [{
     //             type: 'callStatus',
-    //             // predicate: evt => evt.reason === 'callStateChanged' && evt.call.localStreams.video
+    //             predicate: evt => evt.reason === 'callStateChanged' && evt.call.localStreams.video
     //         }])
     //     ]);
-    //     console.log('video', res[1].call.localStreams);
-    //     console.log('localMediaType', res[1].call.localMediaType);
-    //     console.log('--------------------------------------------');
-    //     await sleep(3000);
-    //     const c = await client.findCall(call.callId);
-    //     console.log('CALL', c);
-    //     console.log('#########################################################################################################');
-    //     const res2 = await Promise.all([
-    //         client.toggleVideo(call.callId),
-    //         expectEvents(client, [{
-    //             type: 'callStatus',
-    //             // predicate: evt => evt.reason === 'callStateChanged' && evt.call.localStreams.video
-    //         }])
-    //     ]);
-    //     console.log('video', res2[1].call.localStreams);
-    //     console.log('localMediaType', res2[1].call.localMediaType);
-    //     console.log('--------------------------------------------');
-    //     await sleep(3000);
-    //     const c2 = await client.findCall(call.callId);
-    //     console.log('CALL', c2);
     // });
 
     // it('function: findCall, verifies audio and video stream objects of users', async () => {
@@ -90,26 +70,20 @@ describe('Video Sharing', async function() {
     //     await Promise.all([
     //         client.changeHDVideo(call.callId, true),
     //         expectEvents(client, [{
-    //             type: 'callStatus'
-    //             // predicate: evt => evt.reason === 'sdpConnected' && evt.call.localMediaType.hdVideo
+    //             type: 'callStatus',
+    //             predicate: evt => evt.reason === 'sdpConnected' && evt.call.localMediaType.hdVideo
     //         }])
     //     ]);
-    //     await sleep(3000);
-    //     const res = await client.findCall(call.callId);
-    //     console.log('true', res);
     // });
 
     // it('function: changeHDVideo [OFF], raises event: callStatus with reason: sdpConnected', async () => {
     //     await Promise.all([
     //         client.changeHDVideo(call.callId, false),
     //         expectEvents(client, [{
-    //             type: 'callStatus'
-    //             // predicate: evt => evt.reason === 'sdpConnected' && !evt.call.localMediaType.hdVideo
+    //             type: 'callStatus',
+    //             predicate: evt => evt.reason === 'sdpConnected' && !evt.call.localMediaType.hdVideo
     //         }])
     //     ]);
-    //     await sleep(3000);
-    //     const res = await client.findCall(call.callId);
-    //     console.log('false', res);
     // });
 
     // it('function: toggleVideo [OFF], raises event: callStatus with reason: callStateChanged', async () => {
